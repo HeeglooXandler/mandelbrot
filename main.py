9f4c2cd7028bacf0fdcab295eb79bfb245b99193
HeeglooXandler <156768479+HeeglooXandler@users.noreply.github.com>
2024-01-16 00:29:37 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#import randy
+
+width = np.linspace(-2, 1, 1920)
+height = np.linspace(-2, 2, 1080)
+
+vals = np.meshgrid(width, range(1,1081))[0] + 1j*np.meshgrid(height, range(1,1921))[0].T
+
+v = vals
+while not np.isnan(v).any():
+    mandle = v 
+    v = mandle**2 + vals
+
+mandle = abs(mandle)
+mandle[ mandle > 2 ] = 4
+
+plt.imshow(mandle)
+plt.show()
